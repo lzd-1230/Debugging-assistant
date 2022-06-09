2f57cca6ddbc5456b79fc97c261084e405d99a73
lzd-1230 <576262501@qq.com>
2022-06-09 09:27:45 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ if __name__ == '__main__':
                 # '--paths=D:\\ProgramData\\Anaconda3\\Lib\\site-packages\\PyQt5\\Qt\\bin',
                 # '--paths=D:\\ProgramData\\Anaconda3\\Lib\\site-packages\\PyQt5\\Qt\\plugins',
                 # '--paths=D:\\ProgramData\\Anaconda3\\Lib\\site-packages\\newspaper3k-0.2.6-py3.6.egg\\newspaper',
-                # '--add-data', './style.qss;./style.qss',
-                # '--add-data', 'config/*;config/',
+                '--add-data', './style.qss;./style.qss',
+                '--add-data', 'config/*;config/',
                 # '--add-data', 'image/*;image/',
-                # '--add-data', 'data_save/;data_save/',
+                '--add-data', 'data_save/;data_save/',
                 # '--clean',
                 # '--icon=icon\\icon.ico',
                 'main.py']
