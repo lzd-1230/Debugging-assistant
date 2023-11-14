d3b0f5df4c5831a8fb59fd414f6f384f6307337a
thoelc <623624394@qq.com>
2023-11-14 09:50:14 +0800

    修复串口端口切换问题

--- ui_files/serial_box.py
+++ ui_files/serial_box.py
@@ -2,8 +2,8 @@ from PyQt5.QtWidgets import QComboBox
 from PyQt5 import QtCore
 
 
-class QSerialComboBox(QComboBox):
+class QSerialComBox(QComboBox):
     pop_up = QtCore.pyqtSignal()
     def showPopup(self):
         self.pop_up.emit()
-        super(QSerialComboBox, self).showPopup()
+        super(QSerialComBox, self).showPopup()
